d7da31976d517d360d51b896b694074fe256ee3a
yasin aktimur <yasinaktimur@gmail.com>
2021-08-10 15:00:01 +0300

    http integration works corr.

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,115 @@
+
+/*
+ * HTTPS Secured Client GET Request
+ * Copyright (c) 2022, Yasin Aktimur
+ * All rights reserved.
+ * Connects to WiFi HotSpot. */
+
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
 
-const char* ssid = "yasinaktimur";
-const char* password = "**";
-const char* apikey = "**";
-const char* youraddr = "**";
+/* Set these to your desired credentials. */
+const char *ssid = "yasinaktimur";  //ENTER YOUR WIFI SETTINGS
+const char *password = "12345678";
 
-//Your Domain name with URL path or IP address with path
-String serverName = "https://api.bscscan.com/api?module=account&action=balance&address="+youraddr+"&tag=latest&apikey="+apikey;
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
+
+const char *host = "api.bscscan.com";
+const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
+
+//SHA1 finger print of certificate use web browser to view and copy
+const char fingerprint[] PROGMEM = "8B 36 5F BF DB 6A C8 8A D4 6A B3 0F 27 99 FE 05 AE BB 07 7C";
+//=======================================================================
+//                    Power on setup
+//=======================================================================
 
 void setup() {
+  delay(1000);
   Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("unconnectedify.");
-  }
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
 }
 
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
 void loop() {
-  // Send an HTTP POST request depending on timerDelay
-  if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
+  WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
 
+  Serial.println(host);
 
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName.c_str());
+  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+  httpsClient.setFingerprint(fingerprint);
+  httpsClient.setTimeout(15000); // 15 Seconds
+  delay(1000);
 
-      // Send HTTP GET request
-      int httpResponseCode = http.GET();
-
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      // Free resources
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
+  Serial.print("HTTPS Connecting");
+  int r=0; //retry counter
+  while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+      delay(100);
+      Serial.print(".");
+      r++;
   }
+  if(r==30) {
+    Serial.println("Connection failed");
+  }
+  else {
+    Serial.println("Connected to web");
+  }
+
+  String ADCData, getData, Link;
+  int adcvalue=analogRead(A0);  //Read Analog value of LDR
+  ADCData = String(adcvalue);   //String to interger conversion
+
+  //GET Data
+  Link = "/api?module=account&action=balance&address=ADDDDDDDRESSSSSSSSSSS&tag=latest&apikey=PKEYPKEYPKEYPKEYPKEYPKEYPKEYPKEYPKEYPKEYPKEYPKEY";
+
+  Serial.print("requesting URL: ");
+  Serial.println(host+Link);
+
+  httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+
+  while (httpsClient.connected()) {
+    String line = httpsClient.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+
+  Serial.println("reply was:");
+  Serial.println("==========");
+  String line;
+  while(httpsClient.available()){
+    line = httpsClient.readStringUntil('\n');  //Read Line by Line
+    Serial.println(line); //Print response
+  }
+  Serial.println("==========");
+  Serial.println("closing connection");
+
+  delay(2000);  //GET Data at every 2 seconds
 }
